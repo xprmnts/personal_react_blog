011194502814a7f93ce0259b94b4b214d2c0562e
Abhinay Reddy <xprmnts@protonmail.com>
2017-08-27 09:57:28 -0400

    first commit, initialized express app

---
+++ index.js
@@ -0,0 +1,16 @@
+// node doesn't support ES6 import statements...yet
+// on the server we'll be using CommonJS syntax to handle modules
+const express = require('express');
+
+// initialize an express app object this app object will be used to handle
+// incoming network requests
+const app = express();
+
+
+// temporary route to handle root
+app.get('/', (req, res) => {
+  res.send({ hello: 'world!' });
+});
+
+// in order for express to listen you have to call the listen method
+app.listen(8080);
